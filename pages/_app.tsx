6b17f6988ce50d4b102ff5fb6ef18c2f591c98e4
ridoy <ridoy51305@gmail.com>
2022-04-22 21:41:28 +0600

    updated _app.tsx for service worker installation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,8 +2,24 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import GlobalContext from '../components/hoc/GlobalContext'
 import Layout from '../components/hoc/Layout'
+import { useEffect } from 'react'
 
 function MyApp({ Component, pageProps }: AppProps) {
+  useEffect(() => {
+    if (!('serviceWorker' in navigator)) return
+    
+    navigator.serviceWorker.register('/sw.js').then(
+      function (registration) {
+        console.log(
+          'Service Worker registration successful with scope: ',
+          registration.scope
+        )
+      },
+      function (err) {
+        console.log('Service Worker registration failed: ', err)
+      }
+    )
+  }, [])
   return (
     <GlobalContext {...pageProps}>
       <Layout>
