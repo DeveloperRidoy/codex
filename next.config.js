cee54130f0317cf11a36bf6377851c63a38ad352
ridoy <ridoy51305@gmail.com>
2022-04-22 22:21:39 +0600

    updated files for pwa installation

--- next.config.js
+++ next.config.js
@@ -1,4 +1,10 @@
+const withPWA = require('next-pwa');
+
 /** @type {import('next').NextConfig} */
-module.exports = {
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV !== 'production'
+  },
   reactStrictMode: true,
-}
+})
